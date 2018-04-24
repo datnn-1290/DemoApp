2df82b0cdac2f6485fbef179b076d14ddc620097
nguyennhudat <nguyen.nhu.dat@framgia.com>
2018-04-24 17:00:39 +0700

    entry

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  has_many :entries
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  default_scope -> {order(created_at: :desc)}
   attr_accessor :remember_token
   before_save{email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
@@ -12,6 +22,10 @@ class User < ApplicationRecord
     cost = BCrypt::Engine.cost
     cost = BCrypt::Engine::MIN_COST if ActiveModel::SecurePassword.min_cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def feed
+    Entry.where("user_id = ?", id)
   end
 
   def self.new_token
@@ -30,5 +44,17 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
